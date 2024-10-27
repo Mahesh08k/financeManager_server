298f04f95a307c0a29b2ea98a5149f39107bff8b
Mahesh08k <mash08k@gmail.com>
2024-10-27 22:53:06 +0530

    login api created

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.post(
         });
       }
 
-      /*Create a secur paasowrd using hash and salt for user
+      /*Create a secure paasword using hash and salt for user
         genSalt and hash both are async functions so need to use await before them to get result otherwise we will get error
     */
       const salt = await bcrypt.genSalt(10);
@@ -69,7 +69,63 @@ router.post(
         auth_token,
       });
     } catch (error) {
-      console.log("Internal Server Error", error);
+      console.log("error occured while creating user", err);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+/*Route for validating login for user : api/auth/v1/login */
+router.post(
+  "/login",
+  [
+    body("email", "Enter valid Email").isEmail(),
+    body("password", "Enter valid password").exists(),
+  ],
+  async (req, res) => {
+    try {
+      let success = false;
+      /*get  data from request body */
+      const { email, password } = req.body;
+      /* If there are errors , return bad request and errors */
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      /*Check if given email user exits in database */
+      user = await User.findOne({ email: email });
+      if (!user) {
+        success = false;
+        return res
+          .status(404)
+          .json({ success, Error: "Please Enter valid credentials" });
+      }
+      /* If user exits with given mail id then check if password is correct 
+           compare function takes string and hash as parameter
+           it compares user provided password and password saved in database in hash format
+        */
+      const passwordCheck = await bcrypt.compare(password, user.password);
+      if (!passwordCheck) {
+        success = false;
+        return res
+          .status(404)
+          .json({ success, Error: "Please Enter Valid Credentials" });
+      }
+      /*If password is correct fetch user id to create jwt token*/
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      /*return user jwt token as reward for valid credentials */
+      const jwt_token = jwt.sign(payload, process.env.SECRETKEY);
+      success = true;
+      res
+        .status(200)
+        .json({ success, jwt_token, Message: `Welcome ${user.name}` });
+    } catch (err) {
+      console.log("error occured while logging in", err);
+      res.status(500).send("Internal server error");
     }
   }
 );
